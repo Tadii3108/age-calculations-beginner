bed00ebf620566f1d6c196e0fcccb83d35efff8d
Tadii3108 <tadiwa.zingoni@umuzi.org>
2020-02-05 10:40:43 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+born = input('What year were you born in? ')
+born = int(born)
+age = 2025 - born
+print('You were born in', born)
+print('In 2025 you will be ', age, 'years old!')
+
+years = input('How old are you right now? ')
+years = int(years)
+dog = years * 7
+print('If you were a dog, you would be ', dog, 'years old :D!')
